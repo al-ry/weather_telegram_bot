0d891011864cbf770edfaff548ffff14f69e84d7
Alexander <saska_ryb@mail.ru>
2019-06-27 18:49:35 +0300

    test

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
     include('vendor/autoload.php'); //Подключаем библиотеку
     use Telegram\Bot\Api;
-    $telegram = new Api('885752742:AAF63rND57OidzVAJ3ReDp7qGkX7oVaunBY'); //Устанавливаем токен, полученный у BotFather
+    $telegram = new Api('840599241:AAH6I_Rtq34caNm64rCLJz6mpF0OKHn3iTU'); //Устанавливаем токен, полученный у BotFather
     $result = $telegram -> getWebhookUpdates(); //Передаем в переменную $result полную информацию о сообщении пользователя
     $text = $result["message"]["text"]; //Текст сообщения
     $chat_id = $result["message"]["chat"]["id"]; //Уникальный идентификатор пользователя

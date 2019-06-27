6cc1a2e2493c68f810c4979e97f449663070178e
Alexander <saska_ryb@mail.ru>
2019-06-27 17:56:09 +0300

    test

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
-    echo " hello";
+    include('vendor/autoload.php'); //Подключаем библиотеку
+    use Telegram\Bot\Api; 
+
+    $telegram = new Api('840599241:AAH6I_Rtq34caNm64rCLJz6mpF0OKHn3iTU'); //Устанавливаем токен, полученный у BotFather
+    $result = $telegram -> getWebhookUpdates(); //Передаем в переменную $result полную информацию о сообщении пользователя
+    $text = $result["message"]["text"]; //Текст сообщения
+    $chat_id = $result["message"]["chat"]["id"]; //Уникальный идентификатор пользователя
+    $name = $result["message"]["from"]["username"]; //Юзернейм пользователя
+    $keyboard = [["Узнать погоду"],["Картинка"],["Гифка"]]; //Клавиатура
 ?>

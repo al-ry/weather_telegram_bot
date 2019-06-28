8d8998a62d5e022f11dd6a5ae8e83c41b2612efe
Alexander <saska_ryb@mail.ru>
2019-06-28 11:23:48 +0300

    test

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
     $chat_id = $result["message"]["chat"]["id"]; //Уникальный идентификатор пользователя
     $name = $result["message"]["from"]["username"]; //Юзернейм пользователя
     $keyboard = [["Узнать погоду"],["Избранные города"]]; //Клавиатура
+    $keyboard_forecast = [["Текущая погода"],["Прогноз"]]
 
 //////////////tests
    $country = "Yoshkar-Ola";
@@ -25,6 +26,7 @@
 
     if($text)
     {
+        $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => getWeather($text)]);
         if ($text == "/start")
         {
             if (strlen($name) != 0)
@@ -48,10 +50,6 @@
         {
             $reply = "Введите название населенного пункта";
             $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply ]);
-        }
-        else
-        {
-            $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => getWeather($text)]);
         }
     }
     else

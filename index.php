3f6cfc88a45bd894cf205e3880a1fa970d8d0ac6
Alexander <saska_ryb@mail.ru>
2019-06-28 19:49:59 +0300

    тест

--- index.php
+++ index.php
@@ -65,7 +65,7 @@
            -Temperature: " .$temp_c. " °C , feels like " .$feelslike_temp. " °C
            -Weather: " .$discr. "
            -Humidity: " .$humidity. "%
-           -Pressure: " .floor($pressure % 1.333). " mmHg
+           -Pressure: " .floor($pressure / 1.333). " mmHg
            -Cloudiness: " .$cloud. "%";
       }
       else

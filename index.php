fe4c71b17a0482cdbe04820e66eaad1ec3c187f7
Alexander <saska_ryb@mail.ru>
2019-07-01 10:33:07 +0300

    test db

--- index.php
+++ index.php
@@ -47,7 +47,8 @@
         elseif ($text == "Избранные города")
         {
             ////////db
-
+            $reply_markup = $telegram->replyKeyboardMarkup([ 'keyboard' => $keyboard_city, 'resize_keyboard' => true, 'one_time_keyboard' => false ]);
+            $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup ]);
         }
         elseif ($text == "Добавить город")
         {

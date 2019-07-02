f5f115e36adba0dd3065ceeed2cf8338cbc2e17e
Alexander <saska_ryb@mail.ru>
2019-07-02 19:52:50 +0300

    тест

--- database.php
+++ database.php
@@ -32,5 +32,5 @@ function getUserCommand(MysqliDb $db, string $command): string
 {
     $db->where ("commands", $command);
     $getDataDB = $db->getOne (DB_NAME . ".bot_commands"); 
-    return $getDataDB['commands']; 
+    $getDataDB['commands']; 
 }
--- index.php
+++ index.php
@@ -51,7 +51,7 @@ use Telegram\Bot\Api;
         }
         elseif ($text == "Текущая погода")
         {
-            if (getUserCommand($db, null) == null)
+            if (getUserCommand($db, strlen('')) == '')
             {
                 $reply = "Введите город"; 
                 $data = [

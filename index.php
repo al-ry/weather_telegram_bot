1ef8e07b71c09ecc4c25c79a5c164f2839d87955
Alexander <saska_ryb@mail.ru>
2019-07-02 11:46:34 +0300

    тест

--- index.php
+++ index.php
@@ -5,9 +5,11 @@ require_once('telegramapi.php');
 require_once('weatherapi.php');
 require_once('database.php');
 use Telegram\Bot\Api;
+
+
     
-    $telegram = initToken(); //Устанавливаем токен, полученный у BotFather
-    $result =  userUpdates($telegram); //Передаем в переменную $result полную информацию о сообщении пользовател
+    $telegram = new Api('840599241:AAH6I_Rtq34caNm64rCLJz6mpF0OKHn3iTU'); //Устанавливаем токен, полученный у BotFather
+    $result = $telegram -> getWebhookUpdates(); //Передаем в переменную $result полную информацию о сообщении пользовател
     $text = getText($result); //Текст сообщения
     $chat_id = getUserId($result); //Уникальный идентификатор пользователя
     $name = getUserName($result); //Юзернейм пользователя
--- telegramapi.php
+++ telegramapi.php
@@ -2,27 +2,27 @@
 
 use Telegram\Bot\Api;
 
-function userUpdates($telegram)
+function userUpdates(Api $telegram): void
 {
    $telegram -> getWebhookUpdates();
 }
 
-function initToken()
+function initToken(): Api
 {
     return new Api('840599241:AAH6I_Rtq34caNm64rCLJz6mpF0OKHn3iTU');
 }
 
-function getText($result)
+function getText(object $result)
 {
-    return $result["message"]["text"];
+    $result["message"]["text"];
 }
 
-function getUserId($result)
+function getUserId(object $result)
 {
     return $result["message"]["chat"]["id"];
 }
 
-function getUserName($result)
+function getUserName(object $result)
 {
-    return  $result["message"]["from"]["username"];
+    return $result["message"]["from"]["username"];
 }

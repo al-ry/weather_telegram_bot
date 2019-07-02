49ad0a1b2ab4f957997e9a55879b51e550780287
Alexander <saska_ryb@mail.ru>
2019-07-02 11:24:02 +0300

    тест

--- index.php
+++ index.php
@@ -6,8 +6,8 @@ require_once('weatherapi.php');
 require_once('database.php');
 use Telegram\Bot\Api;
     
-$telegram = new Api('840599241:AAH6I_Rtq34caNm64rCLJz6mpF0OKHn3iTU'); //Устанавливаем токен, полученный у BotFather
-$result = $telegram -> getWebhookUpdates(); //Передаем в переменную $result полную информацию о сообщении пользовател
+    $telegram = initToken(); //Устанавливаем токен, полученный у BotFather
+    $result =  userUpdates($telegram); //Передаем в переменную $result полную информацию о сообщении пользовател
     $text = $result["message"]["text"]; //Текст сообщения
     $chat_id = $result["message"]["chat"]["id"]; //Уникальный идентификатор пользователя
     $name = $result["message"]["from"]["username"]; //Юзернейм пользователя

788b4b9f1dffa4de35872172f8da7b65ac53a59e
Alexander <saska_ryb@mail.ru>
2019-07-04 19:09:16 +0300

    тест

--- weatherapi.php
+++ weatherapi.php
@@ -4,7 +4,7 @@ function getWeatherData(string $city): ?array
 {
     $api = "http://api.apixu.com/v1/forecast.json?key=bd8f380296394c11b8053241192806&q=$city&days=3&lang=ru";
     $weatherData = file_get_contents($api);
-    return $weatherData = json_decode($weatherData, true);
+    return json_decode($weatherData, true);
 }
 
 function getTemperature(array $data): string

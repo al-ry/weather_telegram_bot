a4823998e7c24561c5da9c158470a069bf4aedd2
Alexander <saska_ryb@mail.ru>
2019-07-08 12:24:17 +0300

    тест

--- telegramapi.php
+++ telegramapi.php
@@ -3,10 +3,10 @@
 use Telegram\Bot\Api;
 const apiToken = '840599241:AAH6I_Rtq34caNm64rCLJz6mpF0OKHn3iTU';
 
-function getTelegramApi(): void
+function getTelegramApi()
 {
     $telegram = Api(apiToken);
-    $telegram -> getWebhookUpdates();
+    return $telegram -> getWebhookUpdates();
 }
 
 function getText(object $result): string

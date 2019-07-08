b8fb2c6bf65852310e3232a605b8726fd5fba92d
Alexander <saska_ryb@mail.ru>
2019-07-08 15:19:40 +0300

    тест

--- notifications.php
+++ notifications.php
@@ -1,9 +1,4 @@
 <?php
-    include('vendor/autoload.php');
-    require_once('telegramapi.php');
-    require_once('weatherapi.php');
-    require_once('database.php');
-    require_once('notifications.php');
 
 $cityArray =  getLastCity($db, $chatId);
 $cityName = $cityArray['city_unique'];
